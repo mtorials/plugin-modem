e175f06828517699f7e62addcadf536137aa413a
mtorials <matti.bleckmann@live.com>
2020-10-23 14:05:56 +0200

    Fly speed

--- src/main/kotlin/de/mtorials/modem/commands/FlyCommand.kt
+++ src/main/kotlin/de/mtorials/modem/commands/FlyCommand.kt
@@ -1,5 +1,6 @@
 package de.mtorials.modem.commands
 
+import br.com.devsrsouza.kotlinbukkitapi.dsl.command.arguments.double
 import br.com.devsrsouza.kotlinbukkitapi.dsl.command.command
 import br.com.devsrsouza.kotlinbukkitapi.dsl.command.player
 import br.com.devsrsouza.kotlinbukkitapi.extensions.plugin.WithPlugin
@@ -12,6 +13,11 @@ class FlyCommand(override val plugin: ModemPlugin) : WithPlugin<ModemPlugin> {
             permission = "modem.fly"
             executorPlayer {
                 player.allowFlight = !player.allowFlight
+            }
+            command("speed") {
+                executorPlayer {
+                    player.flySpeed = double(0).toFloat() / 10
+                }
             }
         }
     }
